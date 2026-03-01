cfb4a49f0f84b1126a85494c7c71ae35814f1bef
chen <chen259389@outlook.com>
2026-03-01 17:05:43 +0800

    feat: 更新子仓库；全局注册向导组件

--- vite/src/common/utils.js
+++ vite/src/common/utils.js
@@ -1,5 +1,39 @@
-class Utils {
-  //
+/**
+ * 防抖函数
+ * @param {Function} fn 要执行的函数
+ * @param {number} delay 延迟时间（毫秒）
+ * @returns {Function} 防抖后的函数
+ */
+export const debounce = (fn, delay = 300) => {
+  let timer = null
+  
+  return function(...args) {
+    // 清除之前的定时器
+    if (timer) clearTimeout(timer)
+    
+    // 设置新的定时器
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+      timer = null
+    }, delay)
+  }
 }
 
-export default new Utils();
+export const deepClone = (obj) => {
+  // 如果不是对象或者是null，直接返回
+  if (obj === null || typeof obj !== 'object') {
+    return obj
+  }
+
+  // 根据类型创建新对象
+  const clone = Array.isArray(obj) ? [] : {}
+
+  // 遍历并复制属性
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      clone[key] = deepClone(obj[key])
+    }
+  }
+
+  return clone
+}
--- vite/src/main.js
+++ vite/src/main.js
@@ -4,10 +4,17 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import Transition from './common/components/Transition.vue'
+import Transition from './common/components/Transition/Transition.vue'
+import Guide from './common/components/Guide/Guide.js'
 
 const app = createApp(App)
 
 app.component('MyTransition', Transition)
+
+app.use({
+  install(app) {
+    app.provide('guide', Guide)
+  }
+})
 
 app.use(ElementPlus).mount('#app')
